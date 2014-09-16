a13567550b2a084fa17847bdce837411e77b043e
qiwsir <qiwsir@gmail.com>
2014-09-16 15:12:07 +0800

    import module

---
+++ BasicPython/codes/mmmm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+web = "https://qiwsir.github.io"
+
+def my_name(name):
+    print name
+
+class pythoner:
+    def __init__(self,lang):
+        self.lang = lang
+    def programmer(self):
+        print "python programmer language is: ",self.lang
