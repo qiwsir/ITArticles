7be45a37e908d600dd599ba47b456289c9dfd04d
qiwsir <qiwsir@gmail.com>
2014-10-02 11:11:58 +0800

    analyze the first webpage step by step

---
+++ BasicPython/codes/hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', welcome you to read: www.itdiffer.com')
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
